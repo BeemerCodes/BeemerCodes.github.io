808796226d5f138f38d4da7be51135a37a0fc990
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2024-02-10 00:49:14 +0000

    WIP

--- expert-notes/vite.config.ts
+++ expert-notes/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://beemercodes.github.io/expert-notes/
+//beemercodes.github.io/expert-notes/
 export default defineConfig({
   plugins: [react()],
 });

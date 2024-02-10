8c35fd275af5facb7d2ec10867717a3e795f840d
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2024-02-10 00:57:38 +0000

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/expert-notes/", // Defina o caminho correto aqui
+  base: "/", // Defina o caminho correto aqui
   plugins: [react()],
 });
